33cd260613a79438965c3f3372ebf3a778640de0
LnDevAi <lndubf@gmail.com>
2025-08-21 19:09:02 +0000

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TemplatesPage } from './pages/templates/templates.page';
-import { TemplateViewerPage } from './pages/templates/template-viewer.page';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'templates', pathMatch: 'full' },
-  { path: 'templates', component: TemplatesPage },
-  { path: 'templates/:id', component: TemplateViewerPage },
-];
