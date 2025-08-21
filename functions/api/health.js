5f0ce54744c3812e00521ff1ec216f51e617a471
LnDevAi <lndubf@gmail.com>
2025-08-21 19:08:50 +0000

    Delete functions/api directory

--- functions/api/health.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest() {
-  return new Response(JSON.stringify({ status: 'healthy', timestamp: new Date().toISOString() }), {
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
